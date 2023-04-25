86fb9378178afea8067146da17570e3d1cccc5bd
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-04-25 15:23:09 +0200

    added event listener to grid update button

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const container = document.getElementById("grid-container");
+const gridContainer = document.getElementById("grid-container");
 let slider = document.getElementById('slider')
 let output = document.getElementById('grid-size')
+let gridUpdate = document.getElementById('update-grid')
 output.innerHTML = slider.value + "x" + slider.value
-
-let sliderValue = slider.value
 
 slider.oninput = function () {
     output.innerHTML = this.value + "x" + this.value
 }
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
+  gridContainer.style.setProperty('--grid-rows', rows);
+  gridContainer.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    gridContainer.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(sliderValue, sliderValue);
+// let sliderValue = slider.value
+// makeRows(sliderValue, sliderValue)
+
+gridUpdate.addEventListener('click', function () {
+  removeAllChildNodes(gridContainer);
+  let sliderValue = slider.value;
+  makeRows(sliderValue, sliderValue);
+});
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  };
+};
